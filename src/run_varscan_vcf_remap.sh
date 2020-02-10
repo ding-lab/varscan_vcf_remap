37b7f8a59f503d496a82df6c3754a4ae773a6372
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-02-10 17:05:06 -0600

    Make output directory before running

--- src/run_varscan_vcf_remap.sh
+++ src/run_varscan_vcf_remap.sh
@@ -10,10 +10,28 @@ VCF=$1; shift
 OUT=$1; shift
 XARG="$@"  # https://stackoverflow.com/questions/1537673/how-do-i-forward-parameters-to-other-command-in-bash-script
 
+function test_exit_status {
+    # Evaluate return value for chain of pipes; see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+    rcs=${PIPESTATUS[*]};
+    for rc in ${rcs}; do
+        if [[ $rc != 0 ]]; then
+            >&2 echo Fatal error.  Exiting.
+            exit $rc;
+        fi;
+    done
+}
+
+
 if [ -z $OUT ]; then
     >&2 echo Output VCF not specified.  Quitting.
     exit 1
 fi
+
+OUTD=$(dirname $OUT)
+CMD="mkdir -p $OUTD"
+>&2 echo Running: $CMD
+eval $CMD
+test_exit_status
 
 export PYTHONPATH="/opt/varscan_vcf_remap/src:$PYTHONPATH"
 
@@ -22,20 +40,15 @@ MERGE_FILTER="vcf_filter.py --no-filtered --local-script varscan_vcf_remap.py"  
 # exclude variants reported by just one caller
 MERGE_FILTER_ARGS="$XARG" 
 
-if [ $OUT == '-' ]; then
-    $MERGE_FILTER $VCF $MERGE_FILTER_ARGS 
-else
-    $MERGE_FILTER $VCF $MERGE_FILTER_ARGS > $OUT
+CMD="$MERGE_FILTER $VCF $MERGE_FILTER_ARGS"
+
+if [ $OUT != '-' ]; then
+    CMD="$CMD > $OUT"
 fi
 
-# Evaluate return value for chain of pipes; see https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
-rcs=${PIPESTATUS[*]};
-for rc in ${rcs}; do
-    if [[ $rc != 0 ]]; then
-        >&2 echo Fatal error.  Exiting.
-        exit $rc;
-    fi;
-done
+>&2 echo Running: $CMD
+eval $CMD
+test_exit_status
 
 >&2 echo Written to $OUT
 
