cebbe964e0fe2c062a643984f66936131ef9b0a5
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-02-15 20:25:09 -0600

    pass data directory when starting docker script

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -2,7 +2,9 @@
 
 source docker_image.sh
 
-if [ -z $DATD ]; then
+DATD=$1
+
+if [ "$DATD" ]; then
     # Using python to get absolute path of DATD.  On Linux `readlink -f` works, but on Mac this is not always available
     # see https://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
     ADATD=$(python -c 'import os,sys;print(os.path.realpath(sys.argv[1]))' $DATD)
@@ -12,4 +14,6 @@ if [ -z $DATD ]; then
 fi
 
 # Starting with no mounted volumes:
-docker run $MNT -it $IMAGE
+CMD="docker run $MNT -it $IMAGE /bin/bash"
+>&2 echo Running $CMD
+eval $CMD
